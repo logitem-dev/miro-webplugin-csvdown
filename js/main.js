b8126ab685300bf4f2da3127cf5fff56699a1316
bp-ooya <bp-ooya@kdl.co.jp>
2022-06-28 10:43:20 +0900

    カード変換処理

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ miro.onReady(() => {
 
 
 		  const client_id = '3458764516006800161';
+		  const api_uri = 'https://logitem-dev.herokuapp.com/';
+		  
           // 全イメージオブジェクトの取得
           var allCards = await miro.board.widgets.get({type: 'IMAGE'});
           
@@ -32,6 +34,8 @@ miro.onReady(() => {
               var badge = fromjson[client_id]['badge'] || '';
               var cardColor = fromjson[client_id]['card_color'] || '';
               card.title = staffid + '_' + staffName + '_' + workHour  + '_' + restHour + '_' + badge + '_' + cardColor;
+              card.ur = api_uri + 's3/' + staffid + '.png'
+              miro.board.widgets.update(card);
             }
 
           });
